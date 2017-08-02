4ffde95cc2cc03f232d9920776725e6d9a68bf08
Khoa Nguyen <vankhoa0603@gmail.com>
2017-08-02 15:07:32 +0700

    Finish dev and production environment

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  GOOGLE_CLIENT_ID:
-    "773022877970-l00sj4dueq05tu75ir1iuba6m1i767pi.apps.googleusercontent.com",
-  GOOGLE_CLIENT_SECRET: "_cUTm1kYH_HTceqxvIXwk9oH",
-  mongoURI: "mongodb://user:123456@ds127063.mlab.com:27063/survey",
-  cookieKey: "kashdja918391283sahdas"
-};
