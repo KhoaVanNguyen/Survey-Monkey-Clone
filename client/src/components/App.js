c2bb60ec2dc3e2fbb99c314750533e2d4cb915c1
Khoa Nguyen <vankhoa0603@gmail.com>
2017-08-03 15:55:50 +0700

    ADD react router and css material

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Landing = () => <div>Landing</div>;
+const Dashboard = () => <div> Dashboard </div>;
+const SurveyNew = () => <div> SurveyNew </div>;
 
 const App = () => {
-    return (
-        <div>
-            <p> Hello World  </p>
-        </div>
-    )
-}
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from './reducers'
 
-ReactDOM.render( <App /> , document.querySelector('#root')  )
+const store = createStore( reducers , {}, applyMiddleware());
 
 
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
