5ce4540356ebbd9d81c5ea56479ae39aaa557ee8
Khoa Nguyen <vankhoa0603@gmail.com>
2017-08-08 15:29:48 +0700

    ADD validation for fields except recipient field

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default ({ input, label }) => {
-    return (
-        <div>
-            <label> {label}  </label>
-            <input {...input} />
+export default ({ input, label , meta: {touched, error} }) => {
+    return <div>
+        <label>
+          {" "}{label}{" "}
+        </label>
+        <input  style={{ marginBottom: '5px'  }} {...input} />
+        <div className='red-text' style={{ marginBottom: '20px' }} >
+          {" "}{touched && error} 
         </div>
-    )
+      </div>;
 }
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
 const FIELDS = [
-  { label: "Title", name: "title" },
-  { label: "Subject", name: "subject" },
-  { label: "Body", name: "body" },
+  {
+    label: "Title",
+    name: "title",
+    error: "Please provide title of the survey"
+  },
+  {
+    label: "Subject",
+    name: "subject",
+    error: "Please provide subject of the survey"
+  },
+  { label: "Body", name: "body", error: "Please provide body of the survey" },
   { label: "Recipients", name: "recipients" }
 ];
 class SurveyForm extends Component {
   renderFields() {
-    return FIELDS.map(({label,name}) => {
+    return FIELDS.map(({ label, name }) => {
       return (
         <Field
           key={name}
@@ -28,13 +37,33 @@ class SurveyForm extends Component {
         <h2> SurveyForm </h2>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button className='btn' type="submit"> Submit </button>
+          <Link to="/surveys" className="btn-flat red white-text">
+            Cancel
+            <i className="material-icons left"> cancel </i>
+          </Link>
+          <button className="teal btn-flat white-text right" type="submit">
+            {" "}Next
+            <i className="material-icons right"> done </i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  FIELDS.forEach( ({ name, error  }) => {
+      if (!values[name]) {
+        errors[name] = error;
+      }
+  }  )
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: "surveyForm"
 })(SurveyForm);
